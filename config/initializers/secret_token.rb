4d60471855d6cac603b8b788408376773c380636
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-10-07 21:14:13 -0700

    intial commit for liberty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Liberty::Application.config.secret_key_base = '15b23c1f43fc200984b809a4a7c7eb1e97fe26278c8aad02b5996c722310b2b5b9ee342d293b62bcde004e25c7c10caa22025eaa26673674404c0378bf4522e5'
